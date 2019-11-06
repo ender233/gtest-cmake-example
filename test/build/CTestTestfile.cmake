8a57ecd69f2fa351f5c205fce28d4f0307ca4236
ender233 <ender.liu@outlook.com>
2019-11-07 00:05:38 +0800

    update

--- test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liu/Project/gtest-cmake-example/test
-# Build directory: /home/liu/Project/gtest-cmake-example/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(that-test-I-made "/home/liu/Project/gtest-cmake-example/test/build/testut")
-subdirs("lib/gtest-1.6.0")
--- test/test_project1.cpp
+++ test/test_project1.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "gtest/gtest.h"
-
-#include "project1.h"
-
+#include "project1.h" 
 
 // IndependentMethod is a test case - here, we have 2 tests for this 1 test case
 TEST(IndependentMethod, ResetsToZero) {
@@ -21,42 +19,47 @@ TEST(IndependentMethod, ResetsToZero2) {
 	EXPECT_EQ(0, i);
 }
 
-// The fixture for testing class Project1. From google test primer.
 class Project1Test : public ::testing::Test {
 protected:
-	// You can remove any or all of the following functions if its body
-	// is empty.
+    Project1Test() = default;
+	virtual ~Project1Test() = default;
 
-	Project1Test() {
-		// You can do set-up work for each test here.
-	}
-
-	virtual ~Project1Test() {
-		// You can do clean-up work that doesn't throw exceptions here.
-	}
-
-	// If the constructor and destructor are not enough for setting up
-	// and cleaning up each test, you can define the following methods:
 	virtual void SetUp() {
-		// Code here will be called immediately after the constructor (right
-		// before each test).
 	}
 
 	virtual void TearDown() {
-		// Code here will be called immediately after each test (right
-		// before the destructor).
 	}
-
-	// Objects declared here can be used by all tests in the test case for Project1.
 	Project1 p;
 };
 
-// Test case must be called the class above
-// Also note: use TEST_F instead of TEST to access the test fixture (from google test primer)
 TEST_F(Project1Test, MethodBarDoesAbc) {
 	int i = 0;
 	p.foo(i); // we have access to p, declared in the fixture
 	EXPECT_EQ(1, i);
+}
+
+class StringCmpTest : public ::testing::Test {
+
+};
+
+// google test feature
+TEST_F(StringCmpTest, Demo)
+{
+    char* pszCoderZh = "CoderZh";
+    wchar_t* wszCoderZh = L"CoderZh";
+    std::string strCoderZh = "CoderZh";
+    std::wstring wstrCoderZh = L"CoderZh";
+
+    EXPECT_STREQ("CoderZh", pszCoderZh);
+    EXPECT_STREQ(L"CoderZh", wszCoderZh);
+
+    EXPECT_STRNE("CnBlogs", pszCoderZh);
+    EXPECT_STRNE(L"CnBlogs", wszCoderZh);
+
+    EXPECT_STRCASEEQ("coderzh", pszCoderZh);
+
+    EXPECT_STREQ("CoderZh", strCoderZh.c_str());
+    EXPECT_STREQ(L"CoderZh", wstrCoderZh.c_str());
 }
 
 // }  // namespace - could surround Project1Test in a namespace
