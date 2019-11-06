a90fbe3dda55d541ec8ccc179687bed8c6660160
ender233 <ender.liu@outlook.com>
2019-11-06 20:50:36 +0800

    decouple ut & src

---
+++ src/build/CMakeFiles/project1_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1_lib.dir/project1.cpp.o"
+  "/home/liu/Project/gtest-cmake-example/lib/libproject1_lib.pdb"
+  "/home/liu/Project/gtest-cmake-example/lib/libproject1_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/CMakeFiles/project1_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/home/liu/Project/gtest-cmake-example/lib/libproject1_lib.a"
+)
